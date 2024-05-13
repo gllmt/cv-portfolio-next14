ad9e8bf17e6f818f77fbf4e3224a9e4a64d1833f
Pierre Guillemot <pierregllmt@gmail.com>
2024-05-13 19:07:37 +0200

    projects section is ok

---
+++ components/section-divider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function SectionDivider() {
+  return (
+    <motion.div 
+      className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        type: "tween",
+        delay: 0.1,
+        duration: 0.7, 
+      }}
+    ></motion.div>
+  )
+}
